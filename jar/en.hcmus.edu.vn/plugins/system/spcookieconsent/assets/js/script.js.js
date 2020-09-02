f2d2c4c6a5ef1f802c10019f577732257ab7de2b
huylg <lygiahuy11111998.hcmus@gmail.com>
2020-09-02 17:10:30 +0000

    empty commit :)

---
+++ jar/en.hcmus.edu.vn/plugins/system/spcookieconsent/assets/js/script.js.js
@@ -0,0 +1,20 @@
+/**
+ * @package SP Cookie Consent
+ * @author JoomShaper https://www.joomshaper.com
+ * @copyright Copyright (c) 2010 - 2020 JoomShaper
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 or Later
+*/
+
+jQuery(function ($) {
+    //  Cookie consent
+    $('.sp-cookie-allow').on('click', function(event) {
+        event.preventDefault();
+        
+        var date = new Date();
+        date.setTime(date.getTime() + (30 * 24 * 60 * 60 * 1000));
+        var expires = "; expires=" + date.toGMTString();               
+        document.cookie = "spcookie_status=ok" + expires + "; path=/";
+
+        $(this).closest('#sp-cookie-consent').fadeOut();
+    });
+});
